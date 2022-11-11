c1e20865d2b073e9cd1cdbdfc0bb07d7fd6a01e5
Yovany Cunha <yovany33@gmail.com>
2022-11-11 19:24:42 -0300

    update some config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-/** @type {import('next').NextConfig} */
 const withImages = require('next-images');
 const withPWA = require('next-pwa')({
   dest: 'public',
@@ -7,34 +6,32 @@ const withPWA = require('next-pwa')({
 });
 const withFonts = require('nextjs-fonts');
 
-module.exports = withPWA(
-  withImages(
-    withFonts({
-      reactStrictMode: true,
-      sassOptions: {
-        cssModules: true,
-        includePaths: ['./src'],
-      },
-      images: {
-        domains: ['m.media-amazon.com'],
-        deviceSizes: [360, 375, 768, 1024, 1440],
-        disableStaticImages: true,
-        formats: ['image/webp'],
-      },
-      pageExtensions: ['page.tsx'],
-      fileExtensions: ['jpg', 'jpeg', 'png', 'webp'],
-      webpack(config) {
-        config.module.rules.push({
-          test: /\.svg$/i,
-          issuer: /\.[jt]sx?$/,
-          use: ['@svgr/webpack'],
-        });
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  sassOptions: {
+    cssModules: true,
+    includePaths: ['./src'],
+  },
+  images: {
+    domains: ['m.media-amazon.com'],
+    deviceSizes: [360, 375, 768, 1024, 1440],
+    disableStaticImages: true,
+    formats: ['image/webp'],
+  },
+  pageExtensions: ['page.tsx'],
+  fileExtensions: ['jpg', 'jpeg', 'png', 'webp'],
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
 
-        return config;
-      },
-      env: {
-        STAGE: process.env.STAGE,
-      },
-    })
-  )
-);
+    return config;
+  },
+  env: {
+    STAGE: process.env.STAGE,
+  },
+});
+
+module.exports = nextConfig;
