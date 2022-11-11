4b4d38c6b4906969b7f61649f9e911d7bb96d273
Yovany Cunha <yovany33@gmail.com>
2022-11-11 18:20:49 -0300

    update some config

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+export default class _Document extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>MovieFy - PWA</title>
+          <link
+            rel="shortcut icon"
+            href="alarm-clock.png"
+            type="image/x-icon"
+          />
+          <link rel="manifest" href="/manifest.json" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
